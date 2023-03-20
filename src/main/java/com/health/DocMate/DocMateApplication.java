565c84207541e402401ee402d6d8126a8bb5d9de
Nicolas Neofytou <nickneoph@gmail.com>
2023-03-20 14:44:57 +0200

    initial commit

---
+++ src/main/java/com/health/DocMate/DocMateApplication.java
@@ -0,0 +1,13 @@
+package com.health.DocMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/health/DocMate/DocMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.DocMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
